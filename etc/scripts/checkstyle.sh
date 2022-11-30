dfc9b6a984826314058f4e99b7142d0ea134cb15
Joe Di Pol <joe.dipol@oracle.com>
2022-11-30 13:59:31 -0800

    Remove profiles

--- etc/scripts/checkstyle.sh
+++ etc/scripts/checkstyle.sh
@@ -34,7 +34,7 @@ mvn ${MAVEN_ARGS} checkstyle:checkstyle-aggregate \
     -f ${WS_DIR}/pom.xml \
     -Dcheckstyle.output.format="plain" \
     -Dcheckstyle.output.file="${RESULT_FILE}" \
-    -Pexamples,ossrh-releases > ${LOG_FILE} 2>&1 || (cat ${LOG_FILE} ; exit 1)
+     > ${LOG_FILE} 2>&1 || (cat ${LOG_FILE} ; exit 1)
 
 grep "^\[ERROR\]" ${RESULT_FILE} \
     && die "CHECKSTYLE ERROR" || echo "CHECKSTYLE OK"
