4e111c12940486f3a5b00ad0e57d19b15cc3635c
Maxim Nesen <maxim.nesen@oracle.com>
2024-04-10 22:16:49 +0200

    Fixing helidon-io#8589 (#36)
    Signed-off-by: Maxim Nesen <maxim.nesen@oracle.com>

--- examples/webclient/standalone/src/main/java/io/helidon/examples/webclient/standalone/ServerMain.java
+++ examples/webclient/standalone/src/main/java/io/helidon/examples/webclient/standalone/ServerMain.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, 2023 Oracle and/or its affiliates.
+ * Copyright (c) 2020, 2024 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -41,7 +41,7 @@ public final class ServerMain {
      * @param args starting arguments
      */
     public static void main(String[] args) {
-        startServer();
+        startServer().await();
     }
 
     /**
