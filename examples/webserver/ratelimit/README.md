e6695b9613e91a808faa7dea2dc6195bf6a7cfa7
Joe Di Pol <joe.dipol@oracle.com>
2024-08-15 15:06:20 -0700

    Add webserver ratelimit example

---
+++ examples/webserver/ratelimit/README.md
@@ -0,0 +1,66 @@
+# Helidon SE Rate Limiting Example
+
+This example demonstrates two mechanisms to perform request rate limiting in a Helidon SE application.
+The mechanisms shown here will also work with Helidon MP.
+
+The example does the following:
+
+1. Sets WebServer's `max-concurrent-requests` to 10
+2. Uses a `java.util.concurrent.Semaphore` to limit the number of requests processed concurrently to 5
+
+The server has one endpoint: `sleep` that sleeps for the specified number of seconds. The
+sleep operation is protected by a `Semaphore` to allow no more than five requests to be processed concurrently.
+
+These values are configured in `application.yaml`
+
+## Build and run
+
+Build and start the server:
+```shell
+mvn package
+java -jar target/helidon-examples-webserver-ratelimit.jar
+```
+
+The application logs the limits it is using:
+```
+2024.08.15 14:34:27.694 WebServer maxConcurrentRequests is 10
+2024.08.15 14:34:27.694          Application rate limit is 5
+```
+
+## Exercise the application
+
+Send a burst of 15 concurrent requests to the server:
+```
+for i in $(seq 15); do \
+  curl --noproxy '*' -o /dev/null -s -w "%{http_code}\n" http://localhost:8080/sleep/3 & \
+done | cat
+```
+
+When the 15 concurrent requests hit the server:
+
+* 5 of the requests will be rejected with a 503 because they exceed the server limit of 10
+* 10 of the requests will be accepted by the server
+* The first 5 of those will return a 200 after sleeping for 3 seconds
+* The next 5 requests will then be processed and return a 200 after sleeping for 3 more seconds.
+
+You will see this in the output:
+```asciidoc
+503
+503
+503
+503
+503
+# three second pause
+200
+200
+200
+200
+200
+# three second pause
+200
+200
+200
+200
+200
+200
+```
---
+++ examples/webserver/ratelimit/src/main/java/io/helidon/examples/webserver/ratelimit/Main.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) 2024 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.helidon.examples.webserver.ratelimit;
+
+import java.util.concurrent.Semaphore;
+
+import io.helidon.config.Config;
+import io.helidon.logging.common.LogConfig;
+import io.helidon.webserver.WebServer;
+import io.helidon.webserver.WebServerConfig;
+import io.helidon.webserver.http.HttpRouting;
+import io.helidon.webserver.http.ServerRequest;
+import io.helidon.webserver.http.ServerResponse;
+
+/**
+ * The application main class.
+ */
+public class Main {
+
+    private static final System.Logger LOGGER = System.getLogger(Main.class.getName());
+
+    private static int RATE_LIMIT = 0;
+    private static Semaphore rateLimitSem = null;
+
+    /**
+     * Cannot be instantiated.
+     */
+    private Main() {
+    }
+
+    /**
+     * Application main entry point.
+     * @param args command line arguments.
+     */
+    public static void main(String[] args) {
+
+        // load logging configuration
+        LogConfig.configureRuntime();
+
+        // initialize global config from default configuration
+        Config config = Config.create();
+        Config.global(config);
+
+        WebServerConfig webserverConfig = WebServer.builder()
+                .config(config.get("server"))
+                .routing(Main::routing)
+                .buildPrototype();
+
+        LOGGER.log(System.Logger.Level.INFO, "WebServer maxConcurrentRequests is " + webserverConfig.maxConcurrentRequests());
+
+        WebServer webserver = webserverConfig.build().start();
+
+        System.out.println("WEB server is up! http://localhost:" + webserver.port() + "/sleep");
+    }
+
+    /**
+     * Updates HTTP Routing.
+     */
+    static void routing(HttpRouting.Builder routing) {
+        routing.get("/sleep/{seconds}", Main::sleepHandler);
+        RATE_LIMIT = Config.global().get("app").get("ratelimit").asInt().orElse(20);
+        LOGGER.log(System.Logger.Level.INFO, "         Application rate limit is " + RATE_LIMIT );
+        rateLimitSem = new Semaphore(RATE_LIMIT);
+    }
+
+    /**
+     * Sleep for a specified number of seconds.
+     * The optional path parameter controls the number of seconds to sleep. Defaults to 1
+     *
+     * @param request  server request
+     * @param response server response
+     */
+    private static void sleepHandler(ServerRequest request, ServerResponse response) {
+        int seconds = request.path().pathParameters().first("seconds").asInt().orElse(1);
+
+        try {
+            rateLimitSem.acquire();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            response.send(String.valueOf(sleep(seconds)));
+        } finally {
+            rateLimitSem.release();
+        }
+    }
+
+    /**
+     * Sleep current thread.
+     *
+     * @param seconds number of seconds to sleep
+     * @return number of seconds requested to sleep
+     */
+    private static int sleep(int seconds) {
+        try {
+            Thread.sleep(seconds * 1_000L);
+        } catch (InterruptedException e) {
+            LOGGER.log(System.Logger.Level.WARNING, e);
+        }
+        return seconds;
+    }
+}
