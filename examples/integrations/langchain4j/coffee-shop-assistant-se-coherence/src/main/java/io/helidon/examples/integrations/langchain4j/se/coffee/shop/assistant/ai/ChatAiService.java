194199df8a67b6a34cebbcd284b4a063aab925f5
Arjav Desai <arjav.desai@oracle.com>
2025-08-26 14:07:39 -0500

    fix checkstyle

--- examples/integrations/langchain4j/coffee-shop-assistant-se-coherence/src/main/java/io/helidon/examples/integrations/langchain4j/se/coffee/shop/assistant/ai/ChatAiService.java
+++ examples/integrations/langchain4j/coffee-shop-assistant-se-coherence/src/main/java/io/helidon/examples/integrations/langchain4j/se/coffee/shop/assistant/ai/ChatAiService.java
@@ -27,7 +27,7 @@ import dev.langchain4j.service.SystemMessage;
  * the service registry.
  */
 @Ai.Service
-@Ai.ChatMemoryWindow(value=3,store="coherence")
+@Ai.ChatMemoryWindow(value = 3, store = "coherence")
 public interface ChatAiService {
 
     /**
