13007a50447dfd9d53f184e270860cf7d50dafbe
Tim Quinn <tim.quinn@oracle.com>
2025-09-29 11:43:00 -0500

    Checkstyle

--- examples/telemetry/se-config/src/main/java/io/helidon/examples/telemetry/se/otelconfig/Main.java
+++ examples/telemetry/se-config/src/main/java/io/helidon/examples/telemetry/se/otelconfig/Main.java
@@ -16,20 +16,16 @@
 
 package io.helidon.examples.telemetry.se.otelconfig;
 
-
-import io.helidon.logging.common.LogConfig;
 import io.helidon.config.Config;
+import io.helidon.logging.common.LogConfig;
 import io.helidon.service.registry.Services;
 import io.helidon.webserver.WebServer;
 import io.helidon.webserver.http.HttpRouting;
-
-import io.opentelemetry.api.OpenTelemetry;
 
 /**
  * The application main class.
  */
 public class Main {
-
 
     /**
      * Cannot be instantiated.
@@ -37,13 +33,12 @@ public class Main {
     private Main() {
     }
 
-
     /**
      * Application main entry point.
      * @param args command line arguments.
      */
     public static void main(String[] args) {
-        
+
         // load logging configuration
         LogConfig.configureRuntime();
 
@@ -52,7 +47,7 @@ public class Main {
 
         // Fetch the OpenTelemetry instance from the service registry to make sure it is initialized
         // and assigned as the global OpenTelemetry object early during start-up.
-//        Services.get(OpenTelemetry.class);
+        //        Services.get(OpenTelemetry.class);
 
         WebServer server = WebServer.builder()
                 .config(config.get("server"))
@@ -60,18 +55,16 @@ public class Main {
                 .build()
                 .start();
 
-
         System.out.println("WEB server is up! http://localhost:" + server.port() + "/simple-greet");
 
     }
-
 
     /**
      * Updates HTTP Routing.
      */
     static void routing(HttpRouting.Builder routing) {
         routing
-               .register("/greet", new GreetService())
-               .get("/simple-greet", (req, res) -> res.send("Hello World!")); 
+                .register("/greet", new GreetService())
+                .get("/simple-greet", (req, res) -> res.send("Hello World!"));
     }
 }
