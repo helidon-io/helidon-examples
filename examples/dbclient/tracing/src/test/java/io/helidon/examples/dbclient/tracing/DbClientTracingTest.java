b54d7e0a8ee126d83db1009b9273510f06220c34
Tim Quinn <tim.quinn@oracle.com>
2026-02-19 10:28:35 -0600

    Restore db client tracing example to former state, using the updated core Helidon Jaeger tracing provider (#246)
    * Revert "Allow example to work with newer OpenTelemetry release which has no Jaeger exporter (#237)"
    
    This reverts commit def3e254f47d4013f6aa6f091bb1772e1dd10e3f. A separate PR for core Helidon restores the prior behavior of the Helidon Jaeger tracing provider.

--- examples/dbclient/tracing/src/test/java/io/helidon/examples/dbclient/tracing/DbClientTracingTest.java
+++ examples/dbclient/tracing/src/test/java/io/helidon/examples/dbclient/tracing/DbClientTracingTest.java
@@ -52,13 +52,12 @@ import static org.hamcrest.Matchers.is;
 class DbClientTracingTest {
 
     private static final DockerImageName IMAGE = DockerImageName.parse("cr.jaegertracing.io/jaegertracing/jaeger:2.10.0");
-    private static final int SPAN_DATA_PORT = 4317;
 
     @Container
     @SuppressWarnings("resource")
     private static final GenericContainer<?> CONTAINER = new GenericContainer<>(IMAGE)
-            .withExposedPorts(SPAN_DATA_PORT, 16686)
-            .waitingFor(Wait.forListeningPorts(SPAN_DATA_PORT, 16686)
+            .withExposedPorts(14250, 16686)
+            .waitingFor(Wait.forListeningPorts(14250, 16686)
                                 .withStartupTimeout(Duration.ofMinutes(2)));
 
     private final Http1Client jaegerClient = Http1Client.builder()
@@ -71,7 +70,7 @@ class DbClientTracingTest {
     }
 
     static int tracingPort() {
-        return CONTAINER.getMappedPort(SPAN_DATA_PORT);
+        return CONTAINER.getMappedPort(14250);
     }
 
     @SetUpServer
