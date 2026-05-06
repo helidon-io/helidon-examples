331495d9341f8915eba98f00b7a5f3e18a81e0b5
Shankar Itchapurapu <shankar.itchapurapu@oracle.com>
2026-05-06 17:49:33 +0530

    Fix coherence examples failure when multicast disabled (#301)

--- examples/coherence/src/main/java/io/helidon/examples/coherence/Main.java
+++ examples/coherence/src/main/java/io/helidon/examples/coherence/Main.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Oracle and/or its affiliates.
+ * Copyright (c) 2025, 2026 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,6 +45,9 @@ public class Main {
         // initialize global config from default configuration
         Config config = Config.create();
 
+        // set coherence WKA to force unicast cluster
+        // because cluster startup will fail when multicast is disabled.
+        System.setProperty("coherence.wka", "127.0.0.1");
         Coherence coherence = Coherence.clusterMember();
         coherence.startAndWait();
 
